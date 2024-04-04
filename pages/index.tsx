35d6d5ceff0208a312662867f58948369fd82df5
Volodymyr Alputov <alputov1998@gmail.com>
2024-04-04 15:35:30 +0300

    ADD: text of pagination

--- pages/index.tsx
+++ pages/index.tsx
@@ -50,6 +50,10 @@ export default function Home() {
     setCurrentPage((prevPage) => Math.min(prevPage + 1, totalPages));
   };
 
+  // Calculate the range of items being shown
+  const rangeStart = (currentPage - 1) * itemsPerPage + 1;
+  const rangeEnd = Math.min(currentPage * itemsPerPage, totalItems);
+
   return (
     <main className="flex w-full bg-white min-h-screen flex-col items-center justify-center">
       <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
@@ -96,34 +100,37 @@ export default function Home() {
           </tbody>
         </table>
       </div>
-      <div className="mt-4">
-        <button
-          onClick={handlePreviousPage}
-          disabled={currentPage === 1}
-          className="px-3 py-1 mr-2 bg-green-500 text-white rounded disabled:bg-gray-300 disabled:text-gray-500"
-        >
-          Previous
-        </button>
-        {Array.from({ length: totalPages }, (_, index) => index + 1).map(
-          (pageNumber) => (
-            <button
-              key={pageNumber}
-              onClick={() => setCurrentPage(pageNumber)}
-              className={`px-3 py-1 mx-1 bg-green-500 text-white rounded ${
-                currentPage === pageNumber ? "bg-green-700" : ""
-              }`}
-            >
-              {pageNumber}
-            </button>
-          )
-        )}
-        <button
-          onClick={handleNextPage}
-          disabled={currentPage === totalPages}
-          className="px-3 py-1 ml-2 bg-green-500 text-white rounded disabled:bg-gray-300 disabled:text-gray-500"
-        >
-          Next
-        </button>
+      <div className="mt-4 flex flex-col items-center gap-3">
+        <div>{`Showing ${rangeStart} - ${rangeEnd} of ${totalItems}`}</div>
+        <div>
+          <button
+            onClick={handlePreviousPage}
+            disabled={currentPage === 1}
+            className="px-3 py-1 mr-2 bg-green-500 text-white rounded disabled:bg-gray-300 disabled:text-gray-500"
+          >
+            Previous
+          </button>
+          {Array.from({ length: totalPages }, (_, index) => index + 1).map(
+            (pageNumber) => (
+              <button
+                key={pageNumber}
+                onClick={() => setCurrentPage(pageNumber)}
+                className={`px-3 py-1 mx-1 bg-green-500 text-white rounded ${
+                  currentPage === pageNumber ? "bg-green-700" : ""
+                }`}
+              >
+                {pageNumber}
+              </button>
+            )
+          )}
+          <button
+            onClick={handleNextPage}
+            disabled={currentPage === totalPages}
+            className="px-3 py-1 ml-2 bg-green-500 text-white rounded disabled:bg-gray-300 disabled:text-gray-500"
+          >
+            Next
+          </button>
+        </div>
       </div>
     </main>
   );
