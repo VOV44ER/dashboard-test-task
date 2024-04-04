07946fc0f6920d92069edeee5b3e09ac88722a45
Volodymyr Alputov <alputov1998@gmail.com>
2024-04-04 15:26:36 +0300

    ADD: added pagination

--- pages/index.tsx
+++ pages/index.tsx
@@ -15,14 +15,40 @@ interface Product {
 }
 
 export default function Home() {
-  const [items, setItems] = useState<Product[]>();
-  const [totalItems, setTotalItems] = useState<number>();
-
+  const [items, setItems] = useState<Product[]>([]);
+  const [totalItems, setTotalItems] = useState<number>(0);
+  const [currentPage, setCurrentPage] = useState<number>(1);
+  const itemsPerPage = 10;
   useEffect(() => {
-    fetch("https://dummyjson.com/products?limit=10&skip=0")
+    fetch(
+      `https://dummyjson.com/products?limit=${itemsPerPage}&skip=${
+        (currentPage - 1) * itemsPerPage
+      }`
+    )
       .then((res) => res.json())
-      .then((res) => setItems(res.products));
-  }, []);
+      .then(
+        (res: {
+          limit: number;
+          skip: number;
+          total: number;
+          products: Product[];
+        }) => {
+          setItems(res.products);
+          setTotalItems(res.total);
+          console.log(res);
+        }
+      );
+  }, [currentPage]);
+
+  const totalPages = Math.ceil(totalItems / itemsPerPage);
+
+  const handlePreviousPage = () => {
+    setCurrentPage((prevPage) => Math.max(prevPage - 1, 1));
+  };
+
+  const handleNextPage = () => {
+    setCurrentPage((prevPage) => Math.min(prevPage + 1, totalPages));
+  };
 
   return (
     <main className="flex w-full bg-white min-h-screen flex-col items-center justify-center">
@@ -48,29 +74,56 @@ export default function Home() {
             </tr>
           </thead>
           <tbody>
-            {items?.map(
-              ({ id, title, description, price, category, brand }) => (
-                <tr
-                  key={id}
-                  className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600"
+            {items.map(({ id, title, description, price, category, brand }) => (
+              <tr
+                key={id}
+                className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600"
+              >
+                <th
+                  scope="row"
+                  className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
                 >
-                  <th
-                    scope="row"
-                    className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
-                  >
-                    {title}
-                  </th>
-                  <td className="px-6 py-4">{brand}</td>
-                  <td className="px-6 py-4">{category}</td>
-                  <td className="px-6 py-4">{price}</td>
-                  <td className="px-6 py-4 max-w-[350px] truncate">
-                    {description}
-                  </td>
-                </tr>
-              )
-            )}
+                  {title}
+                </th>
+                <td className="px-6 py-4">{brand}</td>
+                <td className="px-6 py-4">{category}</td>
+                <td className="px-6 py-4">${price}</td>
+                <td className="px-6 py-4 max-w-[350px] truncate">
+                  {description}
+                </td>
+              </tr>
+            ))}
           </tbody>
         </table>
+      </div>
+      <div className="mt-4">
+        <button
+          onClick={handlePreviousPage}
+          disabled={currentPage === 1}
+          className="px-3 py-1 mr-2 bg-green-500 text-white rounded disabled:bg-gray-300 disabled:text-gray-500"
+        >
+          Previous
+        </button>
+        {Array.from({ length: totalPages }, (_, index) => index + 1).map(
+          (pageNumber) => (
+            <button
+              key={pageNumber}
+              onClick={() => setCurrentPage(pageNumber)}
+              className={`px-3 py-1 mx-1 bg-green-500 text-white rounded ${
+                currentPage === pageNumber ? "bg-green-700" : ""
+              }`}
+            >
+              {pageNumber}
+            </button>
+          )
+        )}
+        <button
+          onClick={handleNextPage}
+          disabled={currentPage === totalPages}
+          className="px-3 py-1 ml-2 bg-green-500 text-white rounded disabled:bg-gray-300 disabled:text-gray-500"
+        >
+          Next
+        </button>
       </div>
     </main>
   );
